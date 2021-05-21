33b60aabb9ab78b2c809cafb22aab93e437f32ec
MaiteSM <mai.sanmartin@gmail.com>
2021-05-21 20:19:54 -0300

    puse cap en construcción

--- traducir.R
+++ traducir.R
@@ -4,8 +4,8 @@ library(dplyr)
 archivos <- list.files("docs", pattern = ".html")
 
 for (arch in archivos) {
-	readLines(paste0("docs/", arch)) %>% 
+	readLines(paste0("docs/", arch),encoding = "UTF-8") %>% 
 		str_replace_all("On this page", "En esta página") %>% 
 		str_replace_all("Table of contents", "Tabla de contenidos") %>% 
-		writeLines(paste0("docs/", arch))
+		writeLines(paste0("docs/", arch), useBytes = T)
 }
