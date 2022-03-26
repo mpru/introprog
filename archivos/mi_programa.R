63d13d43c090e42dd527b25b64b5bf6b64b3895b
mpru <marcosprunello@gmail.com>
2022-03-26 16:34:26 -0300

    renovar parte de terminal en capitulo 7

---
+++ archivos/mi_programa.R
@@ -0,0 +1,9 @@
+a <- "¡Hola, Mundo!"
+b <- 3
+d <- 5
+cat("==========================================\n")
+cat("                 RESULTADOS               \n")
+cat("==========================================\n\n")
+cat("El valor de b es ", b, ", mientras que d vale ", d, ".\n\n", sep = "")
+cat("La suma entre ellos es igual a ", b + d, ".\n\n", sep = "")
+cat("Este es un saludo:", a)
